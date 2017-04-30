05e8469ff911487b9d6a606030668fe23265b31c
Anna (Nushyna) <nushyna1208@gmail.com>
2017-04-30 10:48:54 +0300

    New changelist 30.04

---
+++ src/test/java/User/User.java
@@ -0,0 +1,27 @@
+package User;
+
+/**
+ * Created by Anna on 30.04.2017.
+ */
+public class User {
+
+    public String login;
+    public String password;
+    public String mail;
+
+    public User(String login, String password, String mail) {
+        this.login = login;
+        this.password = password;
+        this.mail = mail;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", mail='" + mail + '\'' +
+                '}';
+    }
+}
+
---
+++ src/test/java/User/UserFactory.java
@@ -0,0 +1,15 @@
+package User;
+
+/**
+ * Created by Anna on 30.04.2017.
+ */
+public class UserFactory {
+
+    public  static User getValidUser(){
+        return new User("mytest_test", "337774a", "mytest_test.mail.ru");
+    }
+
+    public static User getNotValidUser(){
+        return new User("mmm", "mmm", "mmm.mail.ru");
+    }
+}
